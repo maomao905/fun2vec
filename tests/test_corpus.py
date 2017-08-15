3d47552e0298676aaa07c540450ff7a1e54cc7eb
maomao905 <easygoing905@gmail.com>
2017-08-15 17:05:33 +0900

    rename to corpus

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -1,12 +1,12 @@
 import unittest
-from dictionary import find_fun_part
+from corpus import find_fun_part
 
 SAMPLE_TEXT1 = '脳内掃除として独り言をつぶやいてる会社員です。  好きなこと：酒、風呂、寝る。最近はブクマとして使ってるような･･･。ここでの発言は私個人の意見であり、所属する組織を代表するものではありません。'
 SAMPLE_TEXT2 = 'Lv3娘との日常生活/洋裁/ゲーム Steam&PS4/FX&株…極稀に雑談配信やゲーム配信やってます。最近はPS4版Divisionをプレイしています。'
 SAMPLE_TEXT3 = 'FateGOありがとう。拡散性ＭＡは滅ぼしましたので乖離性で傭兵メインでやってます。艦これは、矢矧大鳳初風浦風あきつ丸くれ。あ、あと、絶望した。'
 SAMPLE_TEXT4 = 'ビール、箱根駅伝、体操競技、ingress@RES、自転車、ジム、イカ'
 # SAMPLE_TEXT5 = '根っこはAPH祖国島国。うたプリ（セシル）、刀剣（歌仙、蜂須賀）、文アル、FGO、シノアリス他。宝塚は子供の頃から。リアルで話した事がある人にしか話しかけられないので無言フォローすみません。成人済。リフォロは不要です。検索避の為に通常は鍵かけてます。'
-class TestDictionary(unittest.TestCase):
+class TestCorpus(unittest.TestCase):
 
     def test_find_fun_part(self):
         """
