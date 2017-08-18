26105a28a63b0b84e6014d2c7caa128ed85f6bcb
maomao905 <easygoing905@gmail.com>
2017-08-18 22:13:39 +0900

    take the similarity into account to get the middle meaning of word

--- corpus.py
+++ corpus.py
@@ -4,11 +4,14 @@ from util import read_sql
 from wakati import extract_words
 import re
 from itertools import combinations
-from collections import defaultdict
+from collections import defaultdict, Counter
 import word2vec
 import random
 import pickle
 import logging
+from pprint import pprint
+import pandas as pd
+import numpy as np
 
 logging.getLogger().setLevel(logging.INFO)
 logging.basicConfig(format='[%(asctime)s][%(levelname)-5s][%(name)-10s][%(funcName)-10s] %(message)s')
@@ -18,6 +21,7 @@ FILE_SQL = 'sql/filter_interests.sql'
 FILE_DICTIONARY = 'data/dictionary.pkl'
 FILE_WAKATI = 'data/wakati.txt'
 FILE_CORPUS = 'data/corpus.pkl'
+FILE_TOTAL_FUNS = 'data/total_funs_v2.csv'
 MAX_WORD_LENGTH = 10
 REGEX_FUNS = re.compile(r'([ぁ-んァ-ヶー一-龠]{1,%d}(?P<sep>、|,|/|\s|#|・)){2}' % MAX_WORD_LENGTH)
 
@@ -117,6 +121,89 @@ def create_corpus():
         pickle.dump(corpus, f)
     logger.info('Saved corpus of {} sentences in {}'.format(len(corpus), FILE_CORPUS))
 
+def get_similar_words(model, positive, negative=[], cut_sim=0.8, topn=1):
+    res = model.most_similar(positive=positive, negative=negative, topn=topn)
+    sim_words = [w for w, sim in res if sim > cut_sim]
+    return sim_words
+
+def get_best_corpus():
+    """
+    choose good words by self-check => extend those words by gensim most_similar
+    まとめるとあなたは〇〇が好き
+    """
+    with open(FILE_CORPUS, 'rb') as f:
+        corpus = pickle.load(f)
+
+    df = pd.read_csv('data/best_funs.csv', header=None, names=['fun'])
+    # import pdb; pdb.set_trace()
+    model = word2vec.load_model('fun2vec')
+
+    # corpus = [['議論', '英語', 'ビリヤード', 'サッカー', 'プログラミング', 'エンジニア', '機械学習', '耳かき', 'コミュ障', '幼い', 'ドライブ'],
+    # ['カメラ', '機械学習', 'お笑い', 'スケート'], ['アニメ', 'オタク', '東京大学', '公務員'],
+    # ['阪神','PC','鉄道','数学','統計学','音楽','教育','アニメ','仏教','台湾','語学学習'], ['無機化学','有機化学','生化学','薬理学','免疫学','統計学','物理数学'],
+    # ['読書','生物学','統計学','ソフトテニス','ソフトボール'], ['猫','犬','柴犬','ディズニー','ミスチル','サッカー','フットサル']]
+    logger.info('Adding new funs from labeled funs...')
+    for idx, words in enumerate(corpus):
+        # そのプロフィールに存在する正解興味
+        best_funs = df[df.fun.isin(words)].values.flatten().tolist()
+        # 2つ以上正解興味に存在していたら
+        if len(best_funs) >= 2:
+            # 正解興味に一番近いワードを取得
+            words = get_similar_words(model, positive=best_funs, topn=1)
+            # もし近い興味が類似度0.8以上で存在したら
+            if len(words) > 0:
+                # 新たな興味と既存の興味を引き算したものを再度most_similar
+                words2 = get_similar_words(model, positive=best_funs, negative=words, topn=3)
+                df_new = pd.DataFrame(np.array(words+words2).reshape(1, -1).T, columns=['fun'])
+                print(best_funs, words+words2)
+                df = df.append(df_new, ignore_index=True).drop_duplicates()
+            # combs = list(combinations(list(words), 2))
+            # random.shuffle(combs)
+            # 組み合わせの数が多すぎるので、興味の数だけ拡張を行う
+            # combs = combs[:len(words)]
+            # sim_words = []
+            # comb_count = 0
+            # for comb in combs:
+            #     try:
+            #         # cos類似度0.8以上の単語を追加
+            #         sim_words.extend([w for w, sim in model.most_similar(comb, topn=3)])
+            #         comb_count += 1
+            #     except KeyError:
+            #         continue
+            # counter = Counter(sim_words)
+            # print('comb_count', comb_count)
+            # print('sim_words', counter)
+        if (idx + 1) % 10000 == 0:
+            break
+            logger.info('Finished {} sentences'.format(idx+1))
+    df.to_csv(FILE_TOTAL_FUNS, index=False, header=False)
+    logger.info('Done! Saved total funs in {}'.format(FILE_TOTAL_FUNS))
+
+def get_middle_words(model, w1, w2):
+    # w1 = matutils.unitvec(np.array([sim for w, sim in model.most_similar(w1, topn=10)]))
+    # w2 = matutils.unitvec(np.array([sim for w, sim in model.most_similar(w2, topn=10)]))
+    # weight1 = np.mean(matutils.unitvec(n)[sim**2 for w, sim in model.most_similar(w1, topn=10)])
+    weight1 = np.sum([sim for w, sim in model.most_similar(w1, topn=10)])
+    weight2 = np.sum([sim for w, sim in model.most_similar(w2, topn=10)])
+    print('------------------')
+    print(weight1, weight2)
+    # use norm (computationally efficient)
+    w1_vec = model.wv.word_vec(w1, use_norm=True)
+    w2_vec = model.wv.word_vec(w2, use_norm=True)
+    # import pdb; pdb.set_trace()
+    middle = w1_vec - ((w1_vec - w2_vec) * (weight2 / (weight1 + weight2)))
+    # unitvec -> scale a vector to unit length(euclid距離で割る=l2-norm)
+    # np.array([w1_vec, w2_vec]).mean(axis=0)
+    mean = matutils.unitvec(np.array(middle)).astype(REAL) # 重みづけした単語1と単語2の平均vector
+    res = model.similar_by_vector(mean, topn=20)
+    pprint(res)
+
 if __name__ == '__main__':
     # create_dictionary()
-    create_corpus()
+    # get_best_corpus()
+    import sys
+    from gensim import matutils
+    from numpy import exp, log, dot, zeros, outer, random, dtype, float32 as REAL
+    model = word2vec.load_model('fun2vec')
+    args = sys.argv[1:]
+    get_middle_words(model, args[0], args[1])
