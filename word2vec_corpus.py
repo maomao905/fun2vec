a7882a3b1ffe336de9e82d222d23f5cbacd27383
maomao905 <easygoing905@gmail.com>
2017-08-19 16:36:04 +0900

    quit separating profile sentences

--- word2vec_corpus.py
+++ word2vec_corpus.py
@@ -10,25 +10,24 @@ logger = logging.getLogger(__name__)
 
 FILE_CORPUS = 'data/word2vec_corpus.pkl'
 FILE_STOP_WORDS = 'data/stop_words.csv'
+
 def create_corpus():
-    from pprint import pprint
     corpus = []
     session = create_session()
     logger.info('Fetching twitter profile data from DB...')
-    res = session.query(User.description).filter(User.verified==0).limit(10000) # 公式アカウントは除く
-    logger.info('Extracting words from twitter profile...')
+    res = session.query(User.description).filter(User.verified==0).all() # 公式アカウントは除く
+    logger.info('Fetched {} twitter profiles'.format(res.count()))
+    logger.info('Extracting words from twitter profiles...')
     STOP_WORDS = pd.read_csv(FILE_STOP_WORDS, header=None).values.flatten().tolist()
-    SEP_LENGTH = 1000 # 長すぎるとsengmentation failedになるので100で区切り、新しい文章とする
-    for profile in res:
-        words = []
+    for idx, profile in enumerate(res, 1):
         profile = profile[0]
         if profile is None:
             continue
-        for i in range(0, len(profile), SEP_LENGTH):
-            result = extract_words(profile[i:i+SEP_LENGTH], STOP_WORDS)
-            words.extend(result)
+        words = extract_words(profile, STOP_WORDS)
         if len(words) > 0:
             corpus.append(words)
+        if idx % 10000 == 0:
+            logger.info('Finished {} records'.format(idx))
     with open(FILE_CORPUS, 'wb') as f:
         pickle.dump(corpus, f)
     logger.info('Saved corpus of {} sentences in {}'.format(len(corpus), FILE_CORPUS))
