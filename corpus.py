994d3926c3c72bbcf5bf958313a3016f009438fa
maomao905 <easygoing905@gmail.com>
2017-08-27 17:12:13 +0900

    add cluster fun corpus

--- corpus.py
+++ corpus.py
@@ -12,6 +12,7 @@ from pprint import pprint
 import pandas as pd
 import numpy as np
 from flask_script import Manager
+from cluster import cluster_funs
 try:
     import cPickle as pickle
 except ImportError:
@@ -212,6 +213,23 @@ def create_simple_fun2vec_corpus():
     with gzip.open(config['fun2vec']['corpus'], 'wb') as f:
         pickle.dump(user_funs, f)
     logger.info('Saved corpus of {} profiles in {}'.format(len(user_funs), config['fun2vec']['corpus']))
+
+@manager.command
+def create_clustered_fun2vec_corpus():
+    """
+    既存のfun2vec corpusから似た興味をグループ化して、新たなcorpusを作る
+    """
+    with gzip.open(config['fun2vec']['corpus'], 'rb') as f:
+        corpus = pickle.load(f)
+    model = load_model('word2vec')
+    clustered_corpus = []
+    for i, user_funs in enumerate(corpus, 1):
+        clustered_corpus.append(cluster_funs(model, user_funs))
+        if i % 10000 == 0:
+            logger.info('Finished {} profiles'.format(i))
+    with gzip.open(config['fun2vec']['corpus_clustered'], 'wb') as f:
+        pickle.dump(clustered_corpus, f)
+        logger.info('Saved corpus of {} profiles in {}'.format(len(clustered_corpus), config['fun2vec']['corpus_clustered']))
 
 @manager.command
 def create_fun2vec_corpus():
