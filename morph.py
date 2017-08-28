4256620b144d2336fdc9f5b60c5c54e9c705169a
maomao905 <easygoing905@gmail.com>
2017-08-28 11:50:21 +0900

    find close syntax words

--- morph.py
+++ morph.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 REGEX_JA = re.compile(r'[ぁ-んァ-ン一-龥]')
 REGEX_EN = re.compile(r'[a-z]+', re.IGNORECASE)
-REGEX_STOP_CHAR = re.compile(r'^([a-zァ-ン]|[ぁ-ん]{1,2})$', re.IGNORECASE)
+REGEX_STOP_CHAR = re.compile(r'^([ァ-ン一-龥]|[ぁ-ん]{1,2}|[a-z]+)$', re.IGNORECASE)
 
 UNKNOWN_MARK = '*'
 
@@ -58,6 +58,29 @@ def check_en(text):
 
 def filter_feature(features):
     if features[0] == '名詞' and features[1] in ['一般', 'サ変接続', '固有名詞']:
+        if features[2] in ['地域', '人名']:
+            return False
         return True
     else:
         return False
+
+def find_close_words():
+    import difflib
+    from fun2vec import load_model
+    model = load_model('fun2vec')
+    # model.vocab
+    logger.info('Find close words...')
+    vocab = model.index2word
+    for idx, word in enumerate(model.index2word[-100:], 1):
+        # if word is english:
+        #     tagger to get katakana
+        close_words = difflib.get_close_matches(word, vocab, cutoff=0.4)
+        if len(close_words) >= 2:
+            print(word, close_words[1:])
+        # if idx % 50 == 0:
+            # logger.info('{} Finished'.format(idx))
+
+
+
+if __name__ == '__main__':
+    find_close_words()
