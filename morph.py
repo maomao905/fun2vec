2b9dba8727e51ec10f871648a52d4303d382b34c
maomao905 <easygoing905@gmail.com>
2017-09-07 13:48:27 +0900

    表記ゆれを正す

--- morph.py
+++ morph.py
@@ -20,7 +20,7 @@ def extract_words(sentence):
     """
     日本語で名詞 or 形容詞を取得
     """
-    tagger = MeCab.Tagger()
+    tagger = MeCab.Tagger('--dicdir={} --userdic={}'.format('/usr/local/lib/mecab/dic/mecab-ipadic-neologd', 'data/original_dict.dic'))
     tagger.parse('')
     node = tagger.parseToNode(sentence)
     words = []
@@ -66,21 +66,67 @@ def filter_feature(features):
 
 def find_close_words():
     import difflib
+    import pandas as pd
     from fun2vec import load_model
-    model = load_model('fun2vec')
-    # model.vocab
+    result = []
+    model = load_model('word2vec').wv
+    model_fun2vec = load_model('fun2vec').wv
     logger.info('Find close words...')
     vocab = model.index2word
-    for idx, word in enumerate(model.index2word[-100:], 1):
-        # if word is english:
-        #     tagger to get katakana
-        close_words = difflib.get_close_matches(word, vocab, cutoff=0.4)
+    for idx, word in enumerate(model_fun2vec.index2word, 1):
+        close_words = difflib.get_close_matches(word, vocab)
         if len(close_words) >= 2:
-            print(word, close_words[1:])
-        # if idx % 50 == 0:
-            # logger.info('{} Finished'.format(idx))
+            close_words = close_words[1:]
+            for close_word in close_words:
+                try:
+                    sim = model.similarity(word, close_word)
+                    if sim > 0.6:
+                        if model.vocab[word].count > model.vocab[close_word].count:
+                            result.append([word, close_word, sim])
+                        else:
+                            result.append([close_word, word, sim])
+                except KeyError:
+                    pass
+        if idx % 1000 == 0:
+            logger.info('{} Finished'.format(idx))
+    df = pd.DataFrame(result, columns=['replace_word', 'word', 'similarity'])
+    for row in df[df.replace_word.isin(df.word)].itertuples():
+        df.set_value(df[df.replace_word == row.replace_word].index[0], 'replace_word',\
+            df[df.word == row.replace_word].replace_word.values[0])
+    df.drop_duplicates(subset=['replace_word', 'word'], inplace=True)
+    df.to_csv('data/close_word.tsv', sep='\t', index=False)
 
+def create_original_dictionary():
+    import pandas as pd
+    res = []
+    df = pd.read_csv('data/close_word.tsv', sep='\t')
+    for row in df.itertuples():
+        morph = replace_morph(row.word, row.replace_word)
+        if morph:
+            res.append(morph + '\n')
 
+    with open('data/original_dic.csv', 'w') as f:
+        f.writelines(res)
+
+def replace_morph(word, replace_word):
+    """
+    日本語で名詞 or 形容詞を取得
+    """
+    if word != word or word == replace_word:
+        return
+    tagger = MeCab.Tagger()
+    tagger.parse('')
+    try:
+        node = tagger.parseToNode(word)
+    except TypeError:
+        import pdb; pdb.set_trace()
+    while node:
+        if node.surface:
+            features = node.feature.split(',')
+            features[6] = replace_word
+        node = node.next
+    return '{},,,1,'.format(word) + ','.join(features)
 
 if __name__ == '__main__':
-    find_close_words()
+    create_original_dictionary()
+    # replace_morph('寿司', 'お寿司')
