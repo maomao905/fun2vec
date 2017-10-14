07b2392bdb14b4667a90340f5068d40be0db11aa
maomao905 <easygoing905@gmail.com>
2017-10-14 16:58:24 +0900

    modify clean word

--- clean_word.py
+++ clean_word.py
@@ -3,8 +3,9 @@ import re
 PTN_DIGIT_ALL = r'[〇-九\d]'
 
 REGEX_REPLACE = (
-    (re.compile(r'(高校|高等学校)$'), '高校'),
-    (re.compile(r'大学$'), '大学'),
+    (re.compile(r'(高校|高等学校)$'), '高校生'),
+    (re.compile(r'(中学|中学校)$'), '中学生'),
+    (re.compile(r'大学$'), '大学生'),
     (re.compile('新聞'), '新聞'),
     (re.compile('JR'), 'JR'),
     (re.compile('受験'), '受験'),
@@ -13,15 +14,16 @@ REGEX_REPLACE = (
 )
 
 REGEX_EXCLUDE = (
-    (re.compile(r'^{}日目?$'.format(PTN_DIGIT_ALL))),
-    (re.compile(r'^{}年生?$'.format(PTN_DIGIT_ALL))),
-    (re.compile(r'出身$')),
-    (re.compile(r'県$')),
-    (re.compile(r'キロ$')),
-    (re.compile('ごめん')),
-    (re.compile('決定')),
-    (re.compile('注意')),
-    (re.compile(r'^[\d]万?円?$'.format(PTN_DIGIT_ALL))),
+    re.compile(r'^{}日目?$'.format(PTN_DIGIT_ALL)),
+    re.compile(r'^{}年生?$'.format(PTN_DIGIT_ALL)),
+    re.compile(r'出身$'),
+    re.compile(r'県$'),
+    re.compile(r'キロ$'),
+    re.compile('ごめん'),
+    re.compile('決定'),
+    re.compile('注意'),
+    re.compile(r'^[\d]+万?円?$'.format(PTN_DIGIT_ALL)),
+    re.compile(r'^[\d]+(戦|敗|勝)$'),
 )
 
 def clean(word):
